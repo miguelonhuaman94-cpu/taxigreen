4909ca98c13769d0c4ce643b63eadc1907114a0b
Werner Reyes <reyespilcowerner@gmail.com>
2025-08-05 18:00:12 -0500

    Refactor configuración y componentes: utilizar variables de entorno para URL y número de teléfono en WhatsApp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,33 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
+import { loadEnv } from "vite";
+
+const { URL_SITE } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://green-9xc.pages.dev",
+  site: URL_SITE,
   vite: {
     plugins: [
       tailwindcss()
     ],
   },
   integrations: [sitemap()],
+  env: {
+    schema: {
+      PHONE_NUMBER: envField.string({
+        context: "client",
+        access: "public",
+
+      }),
+      URL_SITE: envField.string({
+        context: "client",
+        access: "public",
+      }),
+    }
+  },
 });
